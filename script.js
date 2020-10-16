8b4d642283de4444978215850d38469b3963e6dd
Al9xOleksandr <alex43495@gmail.com>
2020-10-17 00:56:35 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function createTree(height) {
+    for ( var i = 0; i < height ; i++ ) {
+        var star = '*';
+        var space = ' ';
+
+        for ( var j = 1; j <= i; j++ ) {
+            star = star + '**';            
+        }
+           
+        var spacesBefore = space.repeat(height-i-1);
+        star = spacesBefore + star;
+        console.log(star);
+    }
+}
+
+var levels = prompt('Якої висоти повинна бути ялинка?');
+
+createTree(levels);
